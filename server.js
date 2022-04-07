c001294c10663ef390ae07e8ee35d2e7aacb6450
Dev Sharma <devnavla@gmail.com>
2022-04-07 19:23:41 +0530

    forums and single recipes connected to database

--- server.js
+++ server.js
@@ -1,41 +1,7 @@
 import express from 'express';
 import ejs from 'ejs';
-
-// Initialise Express
-var exp = express();
-
-// Render public files (css, js, images)
-exp.use(express.static('public'));
-
-// Port website will run on
-exp.listen(8080);
-
-// Set the view engine to ejs
-exp.set('view engine', 'ejs');
-
-// Render the forum page with a tag
-exp.get('/', function (req, res) {
-	res.render('pages/index.ejs');
-});
-
-exp.get('/add', function (req, res) {
-	res.render('pages/add.ejs');
-});
-
-exp.get('/post', function (req, res) {
-	res.render('pages/post.ejs');
-});
-
-exp.get('/sign-in', function (req, res) {
-	res.render('pages/sign-in.ejs');
-});
-
-exp.get('/sign-up', function (req, res) {
-	res.render('pages/sign-up.ejs');
-});
-
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc, getDocs, addDoc } from 'firebase/firestore';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -48,45 +14,85 @@ const firebaseConfig = {
 	appId: '1:466532377434:web:286f567b43224ae773c963',
 	measurementId: 'G-3HNZQP6W5Z',
 };
-const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
 
-const querySnapshot = await getDocs(collection(db, 'recipes'));
-querySnapshot.forEach((doc) => {
-	console.log(`${doc.id}`);
-	console.log('\t', `${doc.data().username}`);
+// Initialise express
+var app = express();
+
+// Render public files (css, js, images)
+app.use(express.static('public'));
+
+// Port website will run on
+app.listen(8080);
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Render the forum page with a tag
+app.get('/', function (req, res) {
+	res.render('pages/index.ejs');
 });
 
-exp.get('/forum', function (req, res) {
-	res.render('pages/forum.ejs', {
-		tag: req.query.tag,
-		recipes: [
-			{
-				title: 'Chilli Chicken',
-				date: 'January 11th 2019',
-				username: 'Samantha',
-				userImage: 'https://i.pravatar.cc/60?img=1',
-			},
-			{
-				title: 'Chilli Chicken',
-				date: 'January 11th 2019',
-				username: 'Samantha',
-				userImage: 'https://i.pravatar.cc/60?img=1',
-			},
-			{
-				title: 'Chilli Chicken',
-				date: 'January 11th 2019',
-				username: 'Samantha',
-				userImage: 'https://i.pravatar.cc/60?img=1',
-			},
-			{
-				title: 'Chilli Chicken',
-				date: 'January 11th 2019',
-				username: 'Samantha',
-				userImage: 'https://i.pravatar.cc/60?img=1',
-			},
-		],
+app.get('/add', function (req, res) {
+	res.render('pages/add.ejs');
+});
+
+app.get('/post/:id', async (req, res) => {
+	const id = req.params.id;
+	res.render('pages/post.ejs', {
+		recipe: await getRecipe(id),
 	});
 });
+
+app.get('/sign-in', function (req, res) {
+	res.render('pages/sign-in.ejs');
+});
+
+app.get('/sign-up', function (req, res) {
+	res.render('pages/sign-up.ejs');
+});
+
+app.get('/forum', async (req, res) => {
+	const tag = req.query.tag ?? '*';
+	res.render('pages/forum.ejs', {
+		tag: tag,
+		recipes: await getRecipes(tag),
+	});
+});
+
+const getRecipes = async (tag) => {
+	let recipes = [];
+	const querySnapshot = await getDocs(collection(db, 'recipes'));
+
+	querySnapshot.forEach((doc) => {
+		const recipe = doc.data();
+		recipe.id = doc.id;
+		if (Object.values(recipe.tags).includes(tag) || tag === '*') {
+			recipes.push(recipe);
+		}
+	});
+	return recipes;
+};
+
+const getRecipe = async (id) => {
+	const querySnapshot = await getDoc(doc(db, 'recipes', id));
+	const recipe = querySnapshot.data();
+	return recipe;
+};
+
+const addRecipe = async () => {
+	const id = await addDoc(collection(db, 'recipes'), {
+		userImage: 'https://i.pravatar.cc/60?img=1',
+		date: 'January 1st, 2000',
+		tags: { mealType: 'lunch', keyIngredient: 'chicken', region: 'tamil nadu' },
+		title: '',
+		description: '',
+		username: '',
+		ingredients: [],
+		tools: [],
+		instructions: [],
+	});
+	return id;
+};
